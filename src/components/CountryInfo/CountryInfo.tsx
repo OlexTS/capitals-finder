b21e0de4328e79472eece671ea35b1788e49a905
Olexandr <sashokseven@gmail.com>
2026-03-11 23:07:24 +0200

    add details to country

--- src/components/CountryInfo/CountryInfo.tsx
+++ src/components/CountryInfo/CountryInfo.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { Country } from "../../types/country";
 
 interface CountryInfoProps {
@@ -5,21 +6,59 @@ interface CountryInfoProps {
 }
 
 const CountryInfo = ({ data }: CountryInfoProps) => {
+  const [isShowMore, setIsShowMore] = useState(false);
+  const languagesList = data.languages
+    ? Object.values(data.languages).join(", ")
+    : "No data languages";
+  const currensiesList = data.currencies
+    ? Object.values(data.currencies)
+        .map((el) => `${el.name} ${el.symbol}`)
+        .join(", ")
+    : "No data currencies";
+
+  const toggleShowMore = () => {
+    setIsShowMore((prev) => !prev);
+  };
   return (
     <div>
-      
       <img src={data.flags.svg} alt={data.flags.alt} width="200" />
       <h2>{data.name.common}</h2>
       <div>
-        <p>Capital: {data.capital[0]}</p>
+        <p>Capital: {data.capital[0] || "N/A"}</p>
         <p>Region: {data.region}</p>
         <p>Population: {data.population.toLocaleString()}</p>
-        <ul>
-          {data.borders.map((el, idx) => (
-            <li key={idx}>{el}</li>
-          ))}
-        </ul>
+        <div>
+          Borders:
+          {!data.borders || data.borders.length === 0 ? (
+            <span>"There are no land borders"</span>
+          ) : (
+            <ul>
+              {data.borders.map((el) => (
+                <li key={el}>{el}</li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
+      <button onClick={toggleShowMore}>
+        {isShowMore ? "Hide Details" : "Show Details"}
+      </button>
+      {isShowMore && (
+        <div>
+          <p>Currencies: {currensiesList}</p>
+          <p>Languages: {languagesList}</p>
+          <p>
+            Maps: {' '}
+            <a
+              href={data.maps.googleMaps}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Open Google Maps
+            </a>
+          </p>
+        </div>
+      )}
     </div>
   );
 };
--- src/types/country.ts
+++ src/types/country.ts
@@ -1,20 +1,22 @@
 export interface CountryName {
-    common: string;
-    official: string;
+  common: string;
+  official: string;
 }
 
 export interface CountryFlags {
-    png: string;
-    svg: string;
-    alt: string;
+  png: string;
+  svg: string;
+  alt: string;
 }
 
 export interface Country {
-    name: CountryName;
-    capital: string[];
-    flags: CountryFlags;
-    population: number;
-    region: string;
-    borders: string[];
-    
+  name: CountryName;
+  capital: string[];
+  flags: CountryFlags;
+  population: number;
+  region: string;
+  borders?: string[];
+  currencies?: Record<string, {symbol: string; name: string}>;
+  languages: Record<string, string>;
+  maps: { googleMaps: string };
 }
